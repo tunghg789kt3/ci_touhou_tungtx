64341f270bf3197476e8c42e50638bb3c3a89e36
tunghg789kt3 <trantunghg1998@gmail.com>
2017-10-11 19:37:38 +0700

    Added game canvas

---
+++ src/GameCanvas.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GameCanvas extends JPanel{
+
+    public GameCanvas(){
+
+
+    }
+
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -3,8 +3,14 @@ import java.awt.*;
 
 public class GameWindow extends JFrame {
 
+    GameCanvas canvas;
+
     public GameWindow() {
         this.setSize(800,600);
         this.setVisible(true);
+        this.setBackground(Color.gray);
+
+        this.canvas = new GameCanvas();
+        this.setContentPane(this.canvas);
     }
 }
