501df61f8457149feb012adfd2b9611188792fe1
tunghg789kt3 <trantunghg1998@gmail.com>
2017-10-11 20:46:24 +0700

    Added player, backgound, Close option

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,15 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GameWindow extends JFrame {
 
     GameCanvas canvas;
 
     public GameWindow() {
-        this.setSize(800,600);
-        this.setVisible(true);
+        this.setSize(800, 600);
 
         this.canvas = new GameCanvas();
         this.setContentPane(this.canvas);
+        this.setBackground(Color.black);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setResizable(false);
+        this.setLocationRelativeTo(null );
+
+        this.canvas.setVisible(true);
+        this.setVisible(true);
+
     }
+
 }
